62a0a60d029c27c838be788ae5c164e229c700eb
Ariel Marcy <aemarcy@gmail.com>
2018-09-21 13:29:35 +1000

    Sync with desktop files
    Finally getting around to syncing my desktop folder with this GitHub
    repo for better version control

---
+++ Functions/1_prelim_mean_spec.r
@@ -0,0 +1,48 @@
+### Branched from Methodology R Script on 30 Jan 2018
+### Created to analyze preliminary template data to find mean specimen
+
+library(geomorph)
+
+# Read in data file and get it into format that geomorph recognizes
+my_data <- read.csv(file.choose(), header = T) #choose the 18_01_30_AusRodentPhase1.csv file
+
+# This file already has problem LMs deleted, leaving 58 landmarks, 145 semilandmarks, and 86 patches for a total of 289 points
+
+data <- my_data[, 2:(dim(my_data)[2])] #gets rid of LM names for geomorph formatting reasons
+data<-t(data)
+A <- arrayspecs(data, 289, 3) #convert to 3D array with 289 points and 3 dimensions
+
+## Create tables of metadata
+library(stringr)
+
+# create CatNum and scan type metadata
+foo <- my_data[0,]  # gets a list with photo file name
+sp_names <- names(foo[-1])  # creates a list of species names
+categories <- strsplit(sp_names, "_")  #separates important data in list
+my_classifiers <- matrix(unlist(categories), ncol=2, byrow=T)  #reads list into matrix
+colnames(my_classifiers) <- c("Genus", "Species")
+sp_info <- as.data.frame(my_classifiers)  #converts to data frame so can index using $
+sp_info$Both <- with(sp_info, interaction(Genus, Species))
+
+# Create vectors of patch points and semi-landmarks
+patches <- str_detect(u_pt_names, "PAT")
+pat_num <- which(patches == TRUE)
+sli_matrix <- read.csv(file.choose(), header = T) #choose smatrix.csv from methodology paper folder
+
+
+## Analysis time
+
+# Perform procrustes alignment
+Y <- gpagen(A, Proj = TRUE, ProcD = TRUE, curves = sli_matrix, surfaces = pat_num)
+
+
+# Create PCA
+gp <- as.factor(sp_info$Genus)
+col.gp <- rainbow(20)
+names(col.gp) <- levels(gp)
+col.gp <- col.gp[match(gp, names(col.gp))] #colors by Genus
+
+pca <- plotTangentSpace(Y$coords, groups = col.gp, axis1 = 1, axis2 = 2, label = sp_info$Both, verbose = T)
+
+#find mean specimen for template
+findMeanSpec(Y$coords) 
